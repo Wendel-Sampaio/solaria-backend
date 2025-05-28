b624a6086db649e78c20d428321147a19b4238fa
WendelDoa <sswendel11@outlook.com>
2025-05-28 09:38:34 -0300

    first commit

---
+++ src/main/java/com/solaria/app/SolariaApplication.java
@@ -0,0 +1,13 @@
+package com.solaria.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolariaApplication.class, args);
+	}
+
+}
