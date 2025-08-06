fbfbd88b95580b8d450940419a60b85bf7cfde76
WendelDoa <sswendel11@outlook.com>
2025-08-06 16:11:59 -0300

    Add: register route

--- src/main/java/com/solaria/app/controllers/AuthenticationController.java
+++ src/main/java/com/solaria/app/controllers/AuthenticationController.java
@@ -1,10 +1,15 @@
 package com.solaria.app.controllers;
 
 import com.solaria.app.DTOs.AuthenticationDTO;
+import com.solaria.app.DTOs.RegisterDTO;
+import com.solaria.app.entities.User;
+import com.solaria.app.repositories.UserRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthenticationController {
 
+    @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
+        if(this.userRepository.findByEmail(data.email()) != null) {
+            return ResponseEntity.badRequest().build();
+        }
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.email(), encryptedPassword, data.role());
+        this.userRepository.save(newUser);
         return ResponseEntity.ok().build();
     }
 }
