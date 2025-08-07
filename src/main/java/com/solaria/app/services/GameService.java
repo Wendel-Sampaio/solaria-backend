fe31265309bee30d65f05c83ecb8b49679f50e2a
WendelDoa <sswendel11@outlook.com>
2025-08-07 00:25:37 -0300

    Fix: DTO applied

--- src/main/java/com/solaria/app/services/GameService.java
+++ src/main/java/com/solaria/app/services/GameService.java
@@ -1,5 +1,6 @@
 package com.solaria.app.services;
 
+import com.solaria.app.DTOs.GameDTO;
 import com.solaria.app.entities.Game;
 import com.solaria.app.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,10 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
-    public void postGame(Game game) {
-        System.out.print(game);
-        gameRepository.save(game);
+    public void postGame(GameDTO data) {
+        System.out.println(data);
+        Game newGame = new Game(data.title(), data.description(), data.genre(), data.coverImage(), data.releaseDate(), data.creatorUserId());
+        gameRepository.save(newGame);
     }
 
     public List<Game> getAllGames() {
