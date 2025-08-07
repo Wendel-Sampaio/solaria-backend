592b10f33bc034a20a6d24a46cee368094fbcaf5
WendelDoa <sswendel11@outlook.com>
2025-08-07 00:22:45 -0300

    Fix: new user registration fields

--- src/main/java/com/solaria/app/DTOs/RegisterDTO.java
+++ src/main/java/com/solaria/app/DTOs/RegisterDTO.java
@@ -2,5 +2,12 @@ package com.solaria.app.DTOs;
 
 import com.solaria.app.entities.UserRole;
 
-public record RegisterDTO(String email, String password, UserRole role) {
+public record RegisterDTO(
+        String firstName,
+        String lastName,
+        String username,
+        String email,
+        String password,
+        UserRole role
+) {
 }
